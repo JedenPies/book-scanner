f4d34dbdf5d90c51438fb767928986eef1685852
Patryk Dobrowolski <patryk.dobrowolski@gmail.com>
2026-08-28 18:24:41 +0200

    add additional fields to EditScanCommandDto: publisher, publicationYear, publicationPlace, language, and genres

--- scanner-backend/src/main/java/net/patrykdobrowolski/bookscanner/adapter/rest/dto/EditScanCommandDto.java
+++ scanner-backend/src/main/java/net/patrykdobrowolski/bookscanner/adapter/rest/dto/EditScanCommandDto.java
@@ -12,4 +12,9 @@ public class EditScanCommandDto {
 
     private final String title;
     private final List<String> authors;
+    private final String publisher;
+    private final String publicationYear;
+    private final String publicationPlace;
+    private final String language;
+    private final List<String> genres;
 }
